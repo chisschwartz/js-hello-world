51f3c94b869fc49ce87bf61b62a3e58275a669c1
chisschwartz <christopherschwartz559@gmail.com>
2024-04-12 13:28:29 -0500

    Better code

--- hello.js
+++ hello.js
@@ -4,17 +4,24 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World")
+console.log("Hello, Homer")
+console.log("9")
+console.log(9+5)
+console.log("How's the weather down there?"); console.log("a little chilly");
+console.log("He said,\"Nevermore\"")
+console.log("\"woah\"He said"); console.log("what is 9 plus 10?"); console.log("The man responded"); console.log(9+10)
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
+   1. Change the message that is printed. Hello, Homer
+   2. Figure out what the parentheses do. Will the code work without them? It will not
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
+   Both quotations needed. I would guess so that it is seen as writien words and not a command to be executed.
+   4. Remove the semi-colon, ;. Works without semi colon
+   5. Print a number. (Bonus: Print two numbers added together). 
    6. Print multiple messages one after the other.
    7. Print two messages on the same line.
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
